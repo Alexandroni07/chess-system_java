6426e0a2720e53f6ed91392262301ea5071bbf3d
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-07 14:49:15 -0300

    Chess layer and print board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,13 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
+		
+		ChessMatch chesMatch = new ChessMatch();
+		Interface.printBoard(chesMatch.getPieces());
 		
 		
 	}
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,21 @@
 package boardgame;
 
 public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+	}
+	
+	//sem setBoard para que o tabuleiro não possa ser alterado
+	
+	//protected - somente class do mesmo pacote poderão acessar o tabuleiro de uma peça
+	protected Board getBoard() {
+		return board;
+	}
+	
+	
 
 }
