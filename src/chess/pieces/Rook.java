31891686254b57fcca0008b2cbfeb05d5eaa143e
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-08 14:18:45 -0300

    Possible moves

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -15,6 +15,11 @@ public class Rook extends ChessPiece{
 		return "R";
 	}
 	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 	
 
 }
